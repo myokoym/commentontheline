b3704190c2f07e7e0eaf4c40d7919211d45b9e6b
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-04-14 23:55:37 +0900

    Update schema
        % rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414144719) do
+ActiveRecord::Schema.define(version: 20150414145348) do
 
   create_table "texts", force: :cascade do |t|
     t.string   "title"
@@ -21,5 +21,12 @@ ActiveRecord::Schema.define(version: 20150414144719) do
   end
 
   add_index "texts", ["user_id"], name: "index_texts_on_user_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "password",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
